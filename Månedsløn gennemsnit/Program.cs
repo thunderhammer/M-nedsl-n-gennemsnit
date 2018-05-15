f63c9c7f91bfe8c32dcae275ab58eadf7250f3e2
Morten Engemand <engemand@hotmail.com>
2018-05-15 09:49:54 +0200

    Add project files.

---
+++ Månedsløn gennemsnit/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Månedsløn_gennemsnit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
