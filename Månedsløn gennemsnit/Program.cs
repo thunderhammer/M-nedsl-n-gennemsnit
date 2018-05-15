0cbe9d7472ef51142d9642044bb5f54def5ac9d2
Morten Engemand <engemand@hotmail.com>
2018-05-15 10:31:15 +0200

    Commit

--- Månedsløn gennemsnit/Program.cs
+++ Månedsløn gennemsnit/Program.cs
@@ -10,6 +10,31 @@ namespace Månedsløn_gennemsnit
     {
         static void Main(string[] args)
         {
+            double[] a;
+            a = new double[6];
+            a[0] = 10000;
+            a[1] = 14000;
+            a[2] = 9000;
+            a[3] = 500;
+            a[4] = 96000;
+            a[5] = 13001;
+
+            Array.Sort(a);
+
+            double gns = 0;
+            int i;
+
+            for (i = 0; i < a.Length; i++)
+            
+            {
+                gns += a[i];
+                Console.WriteLine(a[i]);
+   
+            }
+ 
+            gns = (gns / i);
+            Console.WriteLine("Gennemsnit: " + gns.ToString("N2"));
+            
         }
     }
 }
